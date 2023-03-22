57a06095e4cf5b4995213c37353e623d0106c080
mmulk2 <mmulk2@uwo.ca>
2023-03-22 00:54:22 -0400

    readding repo

---
+++ js/run-3.js
@@ -0,0 +1,13 @@
+import _exercise3 from './exercise-3.js';
+
+(async () => {
+    console.log(await _exercise3.fetchJoke('GME'));
+    // console.log(await _exercise2.fetchRandomJoke());
+})();
+
+// import _exercise2 from './exercise-3.js';
+
+// (async () => {
+//     // console.log(await icanhazdadjoke.getJokeById('xc21Lmbxcib'));
+//     console.log(await _exercise2.fetchJoke('xc21Lmbxcib'));
+// })();
